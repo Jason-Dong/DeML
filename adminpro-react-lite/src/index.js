2866d470532e3d28f78ec5b30c820d982442658f
Rishabh Krishnan <rishabh.krishnan@berkeley.edu>
2021-10-23 13:26:32 -0700

    added metamask functionality

--- adminpro-react-lite/src/index.js
+++ adminpro-react-lite/src/index.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import indexRoutes from './routes/index.jsx';
 import {  Route, Switch } from 'react-router-dom';
 import { HashRouter } from 'react-router-dom'
-
+import {setupProvider} from 'etherInfra.js'
 import './assets/scss/style.css';
 
 
 
+
+
+
+
+function MetamaskGateway(props) {
+  const [metamask,setMetamask] = useState(false);
+  const result = setupProvider().then((resp)=> {
+    setMetamask(true)
+  })
+  if (metamask){
+    return <HashRouter>
+
+      <Switch>
+        {indexRoutes.map((prop, key) => {
+          return <Route path={prop.path} key={key} component={prop.component} />;
+        })}
+      </Switch>
+
+    </HashRouter>
+  }
+  else {
+    return "Please enable metamask support"
+  }
+}
 ReactDOM.render(
-  <HashRouter>
-  
-    <Switch>
-      {indexRoutes.map((prop, key) => {
-        return <Route path={prop.path} key={key} component={prop.component} />;
-      })}
-    </Switch>
-  
-  </HashRouter>
-  ,document.getElementById('root')); 
+  <MetamaskGateway/>
+  ,document.getElementById('root'));
