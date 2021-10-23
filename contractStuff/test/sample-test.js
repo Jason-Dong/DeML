3dc68156a2e776deb970f896ca0cf49739416ce6
Rishabh Krishnan <rishabh.krishnan@berkeley.edu>
2021-10-23 11:37:25 -0700

    finished script for the most part

--- contractStuff/test/sample-test.js
+++ contractStuff/test/sample-test.js
@@ -3,11 +3,16 @@ const { ethers } = require("hardhat");
 
 describe("Greeter", function () {
   it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+    const Contract = await ethers.getContractFactory("DeML");
+    const contract = await Contract.deploy("test-link", 500, 1000000, 10000000000);
+    await contract.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    expect(await contract.dataset_link()).to.equal("test-link");
+    const competitionEnd = await contract.competition_time();
+    console.log(competitionEnd);
+    const registrationEnd = await contract.registration_time();
+    expect(competitionEnd - registrationEnd).to.equal(1000000);
+    
 
     const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
 
